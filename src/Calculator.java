05241271e1beab1a57f5e53f16f4ed5912d5f421
giorgizena123 <zenaishvili621@gmail.com>
2025-02-04 22:00:02 +0400

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("ნულზე გაყოფა შეუძლებელია.");
+        }
+        return a / b;
+    }
+
+
+    public static double calculateSquareRoot(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("არ შეიძლება უარყოფითი რიცხვის კვადრატული ფესვის გამოთვლა.");
+        }
+        return Math.sqrt(number);
+    }
 }
