04f28b7cc5b51deb9aa84b0652df7b8b544ad09b
giorgizena123 <zenaishvili621@gmail.com>
2025-02-04 22:01:04 +0400

    Update NumberChecker.java

--- src/NumberChecker.java
+++ src/NumberChecker.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class NumberChecker {
+    public static void checkNegative(int number) throws NegativeNumberException {
+        if (number < 0) {
+            throw new NegativeNumberException("რიცხვი უარყოფითია!");
+        }
+    }
 }
