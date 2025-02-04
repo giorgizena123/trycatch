ed7e5b64d859e899b9e8d2342f1136b37b321ace
giorgizena123 <zenaishvili621@gmail.com>
2025-02-04 22:01:24 +0400

    Update NumberConverter.java

--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class NumberConverter {
+    public static int convertToInt(String str) {
+        return Integer.parseInt(str);
+    }
+
+    public static int divide100By(int number) {
+        if (number == 0) {
+            throw new ArithmeticException("ნულზე გაყოფა შეუძლებელია.");
+        }
+        return 100 / number;
+    }
 }
